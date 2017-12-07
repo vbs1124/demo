5e71c61b89c3119275b142a418102b4394631970
vbs1124 <arun.kumar1@intsof.com>
2017-12-07 09:47:56 +0530

    Initial Project Setup

---
+++ DemoWebApp/DemoWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoWebApp.Startup))]
+namespace DemoWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
